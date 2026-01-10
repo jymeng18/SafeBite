2901295608cd427028c7e851ff868d4394a58bd2
jymeng18 <aizeuyn@gmail.com>
2026-01-10 11:45:19 -0800

    react-outer-dom + pages setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,16 @@
  */
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <h1>Hello guys</h1>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>hi</h1>} />
+      <Route path="/app" element={<h1>hello</h1>} />
+      <Route path="*" element={<h1>Hiii</h1>} />
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
