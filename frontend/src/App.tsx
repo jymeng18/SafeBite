ad8044e2cae927e4745fc45fea263f6350a4fb42
jymeng18 <aizeuyn@gmail.com>
2026-01-10 11:48:35 -0800

    tanstack deps + routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,22 @@
  * Author: JM
  */
 
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<h1>hi</h1>} />
-      <Route path="/app" element={<h1>hello</h1>} />
-      <Route path="*" element={<h1>Hiii</h1>} />
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>hi</h1>} />
+        <Route path="/app" element={<h1>hello</h1>} />
+        <Route path="*" element={<h1>Hiii</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 )
 
 export default App
