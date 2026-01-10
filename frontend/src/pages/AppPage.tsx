af517de6fed362e5a61a058543c065e061b4fdf9
jymeng18 <aizeuyn@gmail.com>
2026-01-10 13:51:11 -0800

    imports

--- frontend/src/pages/AppPage.tsx
+++ frontend/src/pages/AppPage.tsx
@@ -7,3 +7,6 @@
  */
 
 import { useState, useEffect } from "react";
+import AppHeader from "@/components/app/AppHeader";
+import { SlidersHorizontal, Map, List, X } from "lucide-react";
+import { Button } from "@/components/ui/button";
