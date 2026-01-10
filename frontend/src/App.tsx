08822a92080734ae71eb941db7feec9e979f2066
jymeng18 <aizeuyn@gmail.com>
2026-01-10 13:46:56 -0800

    landing page done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,8 +9,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import AppHeader from './components/app/AppHeader';
 import NotFound from './pages/NotFound';
+import Index from './pages/Index';
 
 const queryClient = new QueryClient();
 
@@ -18,7 +18,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AppHeader />} />
+        <Route path="/" element={<Index />} />
         <Route path="/app" element={<h1>hello</h1>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
